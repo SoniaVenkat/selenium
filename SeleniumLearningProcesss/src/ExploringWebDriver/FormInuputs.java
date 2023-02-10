286bcf8bca5487ae8b0a7c82613a09b08f31cb49
sonia <sonia@fourbends.com>
2023-02-10 18:22:05 +0530

    changes

--- SeleniumLearningProcesss/src/ExploringWebDriver/FormInuputs.java
+++ SeleniumLearningProcesss/src/ExploringWebDriver/FormInuputs.java
@@ -1,6 +1,8 @@
 package ExploringWebDriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FormInuputs {
@@ -10,6 +12,10 @@ public class FormInuputs {
 		System.getProperty("webdriver.chrome.driver","G:\\Selenium\\chromedriver.exe");
 		driver.get("https://www.jotform.com/form-templates/test");
 		driver.manage().window().maximize();
+		
+		//Using x-path
+		WebElement xpath =driver.findElement(By.xpath("//input[@id='input_3']"));
+		xpath.sendKeys("sample@gmail.com");
+		
 	}
-
 }
